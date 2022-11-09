d9414d96605f3a5f933def59d5ac00bd6e34ced1
BeatHerSppark <peter@halkyon.com>
2022-11-09 20:09:36 +0100

    completed admin page (w/o search)

--- src/components/AllBooks/AllBooks.js
+++ src/components/AllBooks/AllBooks.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -14,22 +15,12 @@ const AllBooks = () => {
   return (
     <Container>
       <h3>Search book:</h3>
-      <Row className="row-cols-4">
-        <Col>
-          <Book />
-        </Col>
-        <Col>
-          <Book />
-        </Col>
-        <Col>
-          <Book />
-        </Col>
-        <Col>
-          <Book />
-        </Col>
-        <Col>
-          <Book />
-        </Col>
+      <Row className="row-cols-3">
+        {books.map((book) => (
+          <Col key={book._id} className="col-12 col-sm-6 col-md-4">
+            <Book book={book} />
+          </Col>
+        ))}
       </Row>
     </Container>
   );
--- src/components/AllBooks/Book/Book.js
+++ src/components/AllBooks/Book/Book.js
@@ -1,22 +1,30 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-const Book = () => {
+const Book = ({ book }) => {
   return (
-    <Card className="my-3" style={{ width: "18rem" }}>
-      <Card.Img
-        variant="top"
-        src="https://www.mountsinai.on.ca/wellbeing/images/image-placeholder/image"
-      />
-      <Card.Body className="bg-dark">
-        <Card.Title>Book</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="dark">See more</Button>
-      </Card.Body>
+    <Card className="my-3 bg-dark" style={{ width: "25rem" }}>
+      <Row className="g-0">
+        <Col className="col-md-4">
+          <Card.Img
+            src={book.bookImg}
+            className="img-fluid rounded-start"
+            alt="Book Image"
+          />
+        </Col>
+        <Col className="col-md-8">
+          <Card.Body>
+            <Card.Title>{book.title}</Card.Title>
+            <Card.Text style={{ maxHeight: "5rem" }} className="overflow-auto">
+              {book.description}
+            </Card.Text>
+            <Button variant="dark">See more</Button>
+          </Card.Body>
+        </Col>
+      </Row>
     </Card>
   );
 };
