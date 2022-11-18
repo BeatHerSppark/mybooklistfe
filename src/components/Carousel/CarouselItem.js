bdccb75264eabd014863b8453e24226cf1458080
BeatHerSppark <peter@halkyon.com>
2022-11-18 18:38:44 +0100

    style change carousel

--- src/components/Carousel/CarouselItem.js
+++ src/components/Carousel/CarouselItem.js
@@ -41,24 +41,26 @@ const CarouselItem = () => {
           <Carousel.Item key={el}>
             <Row className="g-0 justify-content-md-center">
               {el.map((book) => (
-                <Col
-                  key={book._id}
-                  className="mx-3"
-                  style={{ maxWidth: "9rem" }}
-                >
+                <Col key={book._id} style={{ width: "9rem" }}>
                   <Card
                     className="my-3"
                     style={{
                       backgroundColor: "#212121",
                       border: "1px solid #2e2e2e",
-                      maxWidth: "9rem",
+                      width: "9rem",
+                      height: "14rem",
                     }}
                   >
                     <Card.Img
                       src={book.bookImg}
-                      className="img-fluid rounded-start"
+                      className="rounded-start"
                       alt="Book Image"
-                      style={{ maxWidth: "9rem", maxHeight: "12rem" }}
+                      style={{
+                        width: "100%",
+                        height: "100%",
+                        objectFit: "cover",
+                        overflow: "hidden",
+                      }}
                     />
                   </Card>
                 </Col>
